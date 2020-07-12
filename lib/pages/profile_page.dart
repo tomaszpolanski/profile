be408659a447f8f120765716396223d8578eae93
Tomek Polański <polanski.tomek@gmail.com>
2020-07-12 12:50:05 +0200

    Using slivers (#10)

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:profile/pages/profile/section/knowledge_section.dart';
 import 'package:profile/shared/font_icons.dart';
 import 'package:profile/shared/responsive.dart';
 import 'package:profile/shared/text.dart';
@@ -51,226 +52,182 @@ class ProfilePage extends StatelessWidget {
         child: const Icon(Font.skype),
       ),
     ];
-    const knowledge = [
-      KnowledgeCircle(
-        percentage: 1,
-        child: Text('Dart'),
-      ),
-      KnowledgeCircle(
-        percentage: 0.95,
-        child: Text('Kotlin'),
-      ),
-      KnowledgeCircle(
-        percentage: 0.9,
-        child: Text('Java'),
-      ),
-      KnowledgeCircle(
-        percentage: 0.8,
-        child: Text('C#'),
-      ),
-      KnowledgeCircle(
-        percentage: 0.8,
-        child: Text('F#'),
-      ),
-      KnowledgeCircle(
-        percentage: 0.6,
-        child: Text('JS'),
-      ),
-    ];
 
-    return Material(
-      color: Colors.white,
-      child: ListView(
-        children: <Widget>[
-          Container(
-            height: 260,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/droidcon.jpg',
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(
+            child: Container(
+              height: 260,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/droidcon.jpg',
+                  ),
+                  fit: BoxFit.cover,
                 ),
-                fit: BoxFit.cover,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 100),
+                child: _ProfileImage(),
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 100),
-              child: _ProfileImage(),
-            ),
           ),
-          ResponsiveWidget(
-            builder: (context, child, size) {
-              return Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: size == ScreenSize.narrow ? 0 : 120,
-                ),
-                child: child,
-              );
-            },
-            children: <Widget>[
-              const _Card(
-                title: Text('Nutshell'),
-                child: Text('''
+          SliverToBoxAdapter(
+            child: ResponsiveWidget(
+              builder: (context, child, size) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: size == ScreenSize.narrow ? 0 : 120,
+                  ),
+                  child: child,
+                );
+              },
+              children: <Widget>[
+                const CardSection(
+                  title: Text('Nutshell'),
+                  child: Text('''
 I'm Tomek; a mobile & web developer with 13 years' experience across Media, FinTech and Geo-Service industries.
 
 I love that programming requires constant curiosity and self-improvement - I get to solve puzzles and learn new things every single day.
 
 I enjoy roles where I can bring my significant experience to the table, but also have the opportunity to learn from excellent peers, and ideally, share my skills with more junior colleagues.'''),
-              ),
-              _Card(
-                title: const Text('Experience'),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const <Widget>[
-                    SmallParagraph(
-                      title: Text(
-                          '2017 - Present - Groupon: Senior Flutter Developer'),
-                      subtitle: Text('Berlin'),
-                    ),
-                    SizedBox(height: 10, width: 10),
-                    BigParagraph(
-                      title: Text('2011 - 2017 Futurice'),
-                      subtitle: Text('Digital agency, Berlin, Helsinki'),
-                      children: <Widget>[
-                        SmallParagraph(
-                          title: Text('2017 - Friday.de: React.js Developer'),
-                          subtitle: Text(
-                              "Basler's InsurTech venture (pre-launch) - JavaScript/TypeScript"),
-                          children: <Widget>[
-                            Text(
-                                'launching B2C product with web-based React.js solution'),
-                            Text(
-                                'leading Test-Driven Development (TDD) principles for stability and scalability in DACH launch'),
-                            Text(
-                                'voluntarily automated financial statement processing tools, to enable first revenue'),
-                          ],
-                        ),
-                        SmallParagraph(
-                          title: Text(
-                              '2015 - 2016 upday for Samsung: Senior Android Developer'),
-                          subtitle: Text(
-                              'News/media aggregation app, native Android - Java'),
-                          children: <Widget>[
-                            Text(
-                                'rebuilt and relaunched the product using Reactive and Continuous Integration (CI) approach'),
-                            Text(
-                                'wrote, launched and scaled first revenue-enabling features'),
-                            Text(
-                                'coached junior developers in TDD and Reactive Programming'),
-                          ],
-                        ),
-                        SmallParagraph(
-                          title: Text(
-                              '2011 - 2014 HERE (Nokia): Senior Windows Developer'),
-                          subtitle:
-                              Text('Native desktop maps application - C#'),
-                          children: <Widget>[
-                            Text(
-                                'collaborated with Microsoft on R&D, compatibility and global launch'),
-                            Text(
-                                "extended Nokia's C++ map engine to integrate with Windows 8 C# environment"),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    SmallParagraph(
-                      title: Text('2007- 2010 Psiloc: Symbian C++ Developer'),
-                      subtitle: Text('Digital agency, Warsaw'),
-                    ),
-                  ],
                 ),
-              ),
-            ],
-          ),
-          ResponsiveBuilder(
-            builder: (context, size) {
-              final child = size == ScreenSize.wide
-                  ? Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: knowledge,
-                    )
-                  : Center(
-                      child: Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        runAlignment: WrapAlignment.spaceBetween,
-                        alignment: WrapAlignment.spaceAround,
-                        spacing: 40,
-                        runSpacing: 26,
-                        children: knowledge,
+                CardSection(
+                  title: const Text('Experience'),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      SmallParagraph(
+                        title: Text(
+                            '2017 - Present - Groupon: Senior Flutter Developer'),
+                        subtitle: Text('Berlin'),
                       ),
-                    );
-              return Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: size != ScreenSize.wide ? 0 : 120,
+                      SizedBox(height: 10, width: 10),
+                      BigParagraph(
+                        title: Text('2011 - 2017 Futurice'),
+                        subtitle: Text('Digital agency, Berlin, Helsinki'),
+                        children: <Widget>[
+                          SmallParagraph(
+                            title: Text('2017 - Friday.de: React.js Developer'),
+                            subtitle: Text(
+                                "Basler's InsurTech venture (pre-launch) - JavaScript/TypeScript"),
+                            children: <Widget>[
+                              Text(
+                                  'launching B2C product with web-based React.js solution'),
+                              Text(
+                                  'leading Test-Driven Development (TDD) principles for stability and scalability in DACH launch'),
+                              Text(
+                                  'voluntarily automated financial statement processing tools, to enable first revenue'),
+                            ],
+                          ),
+                          SmallParagraph(
+                            title: Text(
+                                '2015 - 2016 upday for Samsung: Senior Android Developer'),
+                            subtitle: Text(
+                                'News/media aggregation app, native Android - Java'),
+                            children: <Widget>[
+                              Text(
+                                  'rebuilt and relaunched the product using Reactive and Continuous Integration (CI) approach'),
+                              Text(
+                                  'wrote, launched and scaled first revenue-enabling features'),
+                              Text(
+                                  'coached junior developers in TDD and Reactive Programming'),
+                            ],
+                          ),
+                          SmallParagraph(
+                            title: Text(
+                                '2011 - 2014 HERE (Nokia): Senior Windows Developer'),
+                            subtitle:
+                                Text('Native desktop maps application - C#'),
+                            children: <Widget>[
+                              Text(
+                                  'collaborated with Microsoft on R&D, compatibility and global launch'),
+                              Text(
+                                  "extended Nokia's C++ map engine to integrate with Windows 8 C# environment"),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      SmallParagraph(
+                        title: Text('2007- 2010 Psiloc: Symbian C++ Developer'),
+                        subtitle: Text('Digital agency, Warsaw'),
+                      ),
+                    ],
+                  ),
                 ),
-                child: _Card(
-                  title: const Text('Programming Languages'),
+              ],
+            ),
+          ),
+          const KnowledgeSection(),
+          SliverToBoxAdapter(
+            child: ResponsiveWidget(
+              builder: (context, child, size) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: size == ScreenSize.narrow ? 0 : 120,
+                  ),
                   child: child,
+                );
+              },
+              children: <Widget>[
+                CardSection(
+                  title: const Text('Human Languages'),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text('Fluent English'),
+                      Text('Native Polish'),
+                      Text('Basic German'),
+                    ],
+                  ),
                 ),
-              );
-            },
+                CardSection(
+                  title: const Text('Education & Certification'),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text('2013 CSM – Certified Scrum Master'),
+                      Text(
+                          '2004 – 2009 Polish-Japanese Institute of Information '
+                          'Technology Warsaw System and network programming'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          ResponsiveWidget(
-            builder: (context, child, size) {
-              return Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: size == ScreenSize.narrow ? 0 : 120,
-                ),
-                child: child,
-              );
-            },
-            children: <Widget>[
-              _Card(
-                title: const Text('Human Languages'),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const <Widget>[
-                    Text('Fluent English'),
-                    Text('Native Polish'),
-                    Text('Basic German'),
-                  ],
-                ),
-              ),
-              _Card(
-                title: const Text('Education & Certification'),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const <Widget>[
-                    Text('2013 CSM – Certified Scrum Master'),
-                    Text('2004 – 2009 Polish-Japanese Institute of Information '
-                        'Technology Warsaw System and network programming'),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          ResponsiveBuilder(
-            builder: (context, size) {
-              final child = size == ScreenSize.wide
-                  ? Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: contacts,
-                    )
-                  : Center(
-                      child: Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        runAlignment: WrapAlignment.spaceBetween,
-                        alignment: WrapAlignment.spaceAround,
-                        spacing: 40,
-                        runSpacing: 20,
+          SliverToBoxAdapter(
+            child: ResponsiveBuilder(
+              builder: (context, size) {
+                final child = size == ScreenSize.wide
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: contacts,
-                      ),
-                    );
-              return Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: size != ScreenSize.wide ? 0 : 120,
-                ),
-                child: _Card(
-                  title: const Text('Contact'),
-                  child: child,
-                ),
-              );
-            },
+                      )
+                    : Center(
+                        child: Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          runAlignment: WrapAlignment.spaceBetween,
+                          alignment: WrapAlignment.spaceAround,
+                          spacing: 40,
+                          runSpacing: 20,
+                          children: contacts,
+                        ),
+                      );
+                return Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: size != ScreenSize.wide ? 0 : 120,
+                  ),
+                  child: CardSection(
+                    title: const Text('Contact'),
+                    child: child,
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -325,8 +282,8 @@ class _ProfileImage extends StatelessWidget {
   }
 }
 
-class _Card extends StatelessWidget {
-  const _Card({
+class CardSection extends StatelessWidget {
+  const CardSection({
     Key key,
     this.title,
     this.child,
@@ -364,80 +321,6 @@ class _Card extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class KnowledgeCircle extends StatefulWidget {
-  const KnowledgeCircle({
-    Key key,
-    this.percentage,
-    this.child,
-  }) : super(key: key);
-
-  final double percentage;
-  final Widget child;
-
-  @override
-  _KnowledgeCircleState createState() => _KnowledgeCircleState();
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties.add(DoubleProperty('percentage', percentage));
-  }
-}
-
-class _KnowledgeCircleState extends State<KnowledgeCircle>
-    with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 900),
-    )..forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      width: 130,
-      child: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          AnimatedBuilder(
-            animation: _controller,
-            builder: (_, __) {
-              return CircularProgressIndicator(
-                value: widget.percentage * _controller.value,
-                strokeWidth: 18,
-              );
-            },
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(14),
-              child: DefaultTextStyle(
-                style: Theme.of(context)
-                    .textTheme
-                    .headline5
-                    .copyWith(fontWeight: FontWeight.bold),
-                child: widget.child,
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
---
+++ lib/shared/appear_sliver.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class SliverValueChanged extends SingleChildRenderObjectWidget {
+  const SliverValueChanged({
+    Key key,
+    Widget child,
+    @required this.onConstraintsChanged,
+    @required this.onGeometryChanged,
+  })  : assert(onConstraintsChanged != null || onGeometryChanged != null),
+        super(key: key, child: child);
+
+  final ValueChanged<SliverConstraints> onConstraintsChanged;
+  final ValueChanged<SliverGeometry> onGeometryChanged;
+
+  @override
+  RenderSliverExample createRenderObject(BuildContext context) =>
+      RenderSliverExample(
+        onConstraintsChanged: onConstraintsChanged,
+        onGeometryChanged: onGeometryChanged,
+      );
+}
+
+class RenderSliverExample extends RenderSliverSingleBoxAdapter {
+  RenderSliverExample({
+    @required this.onConstraintsChanged,
+    @required this.onGeometryChanged,
+    RenderBox child,
+  }) : super(child: child);
+
+  final ValueChanged<SliverConstraints> onConstraintsChanged;
+  final ValueChanged<SliverGeometry> onGeometryChanged;
+
+  @override
+  void performLayout() {
+    if (child == null) {
+      geometry = SliverGeometry.zero;
+      return;
+    }
+    child.layout(constraints.asBoxConstraints(), parentUsesSize: true);
+    double childExtent;
+    switch (constraints.axis) {
+      case Axis.horizontal:
+        childExtent = child.size.width;
+        break;
+      case Axis.vertical:
+        childExtent = child.size.height;
+        break;
+    }
+    assert(childExtent != null);
+    final double paintedChildSize =
+        calculatePaintOffset(constraints, from: 0, to: childExtent);
+    final double cacheExtent =
+        calculateCacheOffset(constraints, from: 0, to: childExtent);
+
+    assert(paintedChildSize.isFinite);
+    assert(paintedChildSize >= 0.0);
+    geometry = SliverGeometry(
+      scrollExtent: childExtent,
+      paintExtent: paintedChildSize,
+      cacheExtent: cacheExtent,
+      maxPaintExtent: childExtent,
+      hasVisualOverflow: childExtent > constraints.remainingPaintExtent ||
+          constraints.scrollOffset > 0.0,
+    );
+    if (onConstraintsChanged != null) {
+      onConstraintsChanged(constraints);
+    }
+    if (onGeometryChanged != null) {
+      onGeometryChanged(geometry);
+    }
+    setChildParentData(child, constraints, geometry);
+  }
+}
